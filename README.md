3afb67374a0e688dfd3952ccb5fb6cb8f54191db
Grassien <fredgsn59@gtmail.com>
2022-02-21 13:32:34 +0100

    Update 21/02

---
+++ README.md
@@ -0,0 +1,1 @@
+Formation_Afpa_Java
---
+++ code/MemoCode/src/main/travail/ProgrammationObjet/Voiture.java
@@ -0,0 +1,41 @@
+package main.travail.ProgrammationObjet;
+
+public class Voiture {
+    // Propriétés
+    String marque;
+    String immatriculation;
+    int nbPortes = 4;
+
+    // Méthodes (comportements)
+
+    // Mehode sans retour (void) et sans paramétres
+    void klaxonner(){
+        System.out.println("Tut tut");
+    }
+
+    // Methode avec retour
+    int donnerVitesse(){
+        return 100;
+    }
+
+    // Methode avec 1 parametre
+    int donnerNouvelleVitesse(int vitesseActuelle){
+        return vitesseActuelle+10;
+    }
+
+    // Methode avec plusieurs parametres
+    int changerVitesse(int vitesseActuelle, boolean accelerer, int ecartVitesse){
+        if(accelerer){
+            System.out.println("J'accelere");
+            vitesseActuelle +=ecartVitesse;
+        }
+        else {
+            System.out.println("Je ralentis");
+            vitesseActuelle -=ecartVitesse;
+        }
+
+        return vitesseActuelle;
+    }
+
+
+}
